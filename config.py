d8ecbcdd0c6aefe8b3bac4bf207dd0bd2948d64f
Prisyazhnyuk Alexey <91311426+PrisyazhnyukAlexey@users.noreply.github.com>
2023-03-31 11:22:17 +0300

    Update and rename config.py.example to config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+BASE_FOLDER = 'images'
+
+FIGMA_PROJECTS = {
+    'Name': '87601356',
+}
+
+FIGMA_API_TOKEN: str = "%figd_-BEoyNjVXCajOgFIoV0GBuqYBmVF4GJ-cf1fOmva%"
+
+ZEPLIN_API_TOKEN: str = "%token%"
