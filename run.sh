a72ec67bf3688691a1a415954909624a451c65c3
Prisyazhnyuk Alexey <91311426+PrisyazhnyukAlexey@users.noreply.github.com>
2023-03-31 11:22:46 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ cd /Users/mike/work/git-repos/stuff/zeplin-download-recent
 python3 -mvenv .venv
 source .venv/bin/activate
 
-python zeplin-download-screens.py
+#python zeplin-download-screens.py
 python figma-download-screens.py
